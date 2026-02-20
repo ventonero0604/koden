0eb36e9f11fce10b98491cd2709f4f110c93ed60
anagamin <burningsunburst@yahoo.co.jp>
2026-02-20 11:43:38 +0900

    css修正

--- components/menuLinks.php
+++ components/menuLinks.php
@@ -1,4 +1,4 @@
-<li><a href="/">HOME</a></li>
-<li><a href="/#exhibition">現在開催中の展示</a></li>
-<li><a href="/exhibition/">これまでの展示</a></li>
-<li><a href="/news/">お知らせ</a></li>
+<li><a href="<?php echo home_url(); ?>">HOME</a></li>
+<li><a href="<?php echo home_url('/#exhibition'); ?>">現在開催中の展示</a></li>
+<li><a href="<?php echo home_url('/exhibition/'); ?>">これまでの展示</a></li>
+<li><a href="<?php echo home_url('/news/'); ?>">お知らせ</a></li>
